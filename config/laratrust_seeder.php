fb7c9e4b5a4c0e3d50e76567c7fe624d38c72f69
NHKhanq <nhkhang061203@gmail.com>
2024-09-16 07:33:44 +0700

    laratrust

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,32 @@
+<?php
+//https://laratrust.santigarcor.me/docs/8.x/usage/seeder.html#seeder-configuration-file
+return [
+    /**
+     * Control if the seeder should create a user per role while seeding the data.
+     */
+    'create_users' => false,
+
+    /**
+     * Control if all the laratrust tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+
+    'roles_structure' => [
+        'admin' => [
+            'users' => 'c,r,u,d',
+            'payments' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'user' => [
+            'users' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ]
+    ],
+
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
